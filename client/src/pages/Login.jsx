45c22d97b845c6cb2feb6be9460d3415575615c5
Loveesh Gulati <loveeshgulati007@gmail.com>
2025-04-01 10:06:20 +0530

    Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,73 @@
- import React from 'react'
- 
+ import React, {useEffect} from 'react'
+ import {useForm} from 'react-hook-form'
+ import { useNavigate} from 'react-router-dom';
+ import Textbox from '../components/Textbox'
  const Login = () => {
-   return (
-    <h1 class="text-3xl font-bold underline">
-    Login
-  </h1>
-   )
- }
+  const user = "";
+  const 
+  {register,
+    handleSubmit,
+    formState : {errors},
+  } = useForm();  
+  const navigate = useNavigate();
+  const submitHandler = async (data) => {
+    console.log("submit")
+  }
+  useEffect(() => {
+    user && navigate('/dashboard');
+  }, [user]);
+  return (
+  <div className = "w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+    <div className="w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center">
+      {/* Left side */}
+      <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+      <div className="w-full md:max-w-lg 2x1:max-w-3x1 flex flex-col items-center justify-center gap-5 md:gap-y-10 2x1:-mt-20">
+        <span className="flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base bordergray-300 text-gray-600"> Manage all your tasks in one place</span>
+        <p className="flex flex-col gap-0 md:gap-4 text-4x1 md:text-6x1 2x1:text-7x1 font-black text-center text-blue-700">
+          <span>Cloud-Based</span>
+          <span>Task Manager</span>
+        </p>
+        <div>
+          <div className="circle rotate-in-up-left"></div>
+        </div>
+      </div>
+
+      </div>
+      {/* Right side */}
+      <div className="w-full md:w-1/3 p-4 md:p-1 flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit(submitHandler)}
+      className="form-container w-full md:w-[400px] flex flex-col gap-y-8 bg-white px-10 pt-14 pb-14">
+        <div className="">
+          <p className="text-blue-600 text-3x1 font-bold text-center">Welcome Back!
+          </p>
+          <p className="text-center text-base text-gray-700">
+            Keep all your crendential safe.
+          </p>
+
+        </div>
+        <div className="flex flex-col gap-y-5">
+          <Textbox
+          placeholder="email@example.com" 
+          type="email"
+          name="email"
+          label="Email Address"
+          className="w-full rounded-full"
+          register={register("email",{required: "Email Address is required!"} )}
+          error={errors.email? errors.email.message : ""}
+
+          />
+        </div>
+
+      </form>
+
+
+      </div>
+
+    </div>
+  </div>
+  );
+
+   
+ };
  
  export default Login
