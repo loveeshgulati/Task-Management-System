9407548fde6b296b271fe7dea29eda522bc3e875
sartikmj <sartikmj@gmail.com>
2025-04-07 12:33:57 +0530

    Dashboard,Navbar,NotificationPanel

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
-import{BrowserRouter} from "react-router-dom"
-
-import './index.css';  
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux";
+import './index.css';
 import App from './App.jsx';
+import store from "./redux/store.js";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
-    <App />
-    </BrowserRouter>
-   
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
