96ff80866e1aa2627031feef7c2a2dd8d1fc5229
Madhav Agrawal <your-emailexample.com>
2025-04-15 09:57:06 +0530

    Dashboard, User Info

---
+++ client/src/assets/data.js
@@ -0,0 +1,33 @@
+export const summary ={
+    totalTasks: 10,
+    last10Task:[
+
+    ],
+    users:[
+
+    ],
+    tasks:{
+        todo:6,
+        "in progress":3,
+        completed: 1,
+    }, 
+};
+
+export const chartData=[
+{
+    name:"high",
+    total:2400,
+},
+{
+    name:"medium",
+    total:2210,
+},
+{
+    name:"low",
+    total:"2290",
+}
+];
+
+export const tasks=[
+
+];
